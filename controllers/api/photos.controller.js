42fa6b8947f121b5113b18b099ab6dd234419621
Rudi Thoma <thoma.rudi@gmail.com>
2016-04-20 21:37:21 +0200

    Addind Winston

--- controllers/api/photos.controller.js
+++ controllers/api/photos.controller.js
@@ -8,6 +8,11 @@ var AWS_SECRET_KEY = "SKHD4jPcqZtc/G+KTaXU8Aed1a59K/q7jHAzbAxY";
 var S3_BUCKET = "rudithoma";
 var multer  = require('multer');
 var fs = require('fs');
+var winston = require('winston');
+  winston.add(winston.transports.File, { filename: 'logger.log' });
+  winston.remove(winston.transports.Console);
+
+
 aws.config.update({
     signatureVersion: 'v4'
 });
@@ -34,11 +39,21 @@ router.post('/upload_photo', upload.single('image'), function(req, res) {
                 Body:fileBuffer
             };
             s3.putObject(s3_params,function(err, data) {
-                if (err) console.log(err, err.stack); // an error occurred
-                else     console.log(data);
-                
+                if (err) {
+                    winston.error(err, err.stack); // an error occurred
+                }
+                else{
+                     winston.log(data);
+                     winston.log("Going to delete an existing file");
+                        fs.unlink(files.destination+files.filename, function(err) {
+                           if (err) {
+                               winston.error(err);
+                           }
+                           winston.log("File deleted successfully!");
+                        });
+                }
             });
-     
+
         if (berr) {
              res.status(400).send(txterr);
         }else{
@@ -56,33 +71,9 @@ function registerPhoto(req, res) {
             res.sendStatus(200);
         })
         .catch(function (err) {
+			winston.error(err);
             res.status(400).send(err);
         });
-}
-
-function sign_s3(req,res) {
-     aws.config.update({accessKeyId: AWS_ACCESS_KEY, secretAccessKey: AWS_SECRET_KEY});
-    var s3 = new aws.S3();
-    var s3_params = {
-        Bucket: S3_BUCKET,
-        Key: req.query.file_name,
-        Expires: 600,
-        ContentType: req.query.file_type,
-        ACL: 'public-read'
-    };
-    s3.getSignedUrl('putObject', s3_params, function(err, data){
-        if(err){
-            console.log(err);
-        }
-        else{
-            var return_data = {
-                signed_request: data,
-                url: 'http://'+S3_BUCKET+'.s3.amazonaws.com/'+req.query.file_name
-            };
-            res.write(JSON.stringify(return_data));
-            res.end();
-        }
-    });
 }
 
 function getCurrentPhoto(req, res) {
@@ -93,10 +84,12 @@ function getCurrentPhoto(req, res) {
             if (photo) {
                 res.send(photo);
             } else {
+				winston.error(err);
                 res.sendStatus(404);
             }
         })
         .catch(function (err) {
+			winston.error(err);
             res.status(400).send(err);
         });
 }
