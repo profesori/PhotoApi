606885e6f5196833ea8450d72073b2da3d481710
profesori <thoma.rudi@gmail.com>
2016-04-21 17:18:51 +0200

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 app.use(session({ secret: config.secret, resave: false, saveUninitialized: true }));
 
 // use JWT auth to secure the api
-app.use('/api', expressJwt({ secret: config.secret }).unless({ path: ['/api/users/authenticate', '/api/users/register','/api/photos/upload_photo'] }));
+app.use('/api', expressJwt({ secret: config.secret }).unless({ path: ['/api/users/authenticate', '/api/users/register','/api/photos/add'] }));
 
 // routes
 //app.use('/login', require('./controllers/login.controller'));
